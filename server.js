3c62d1a6d6b9d925a782f3126ba939d680140d5a
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-05-20 13:32:29 +0300

    .png added to filename

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use(async (ctx) => {
     if (file) {
       const link = await new Promise((resolve, reject) => {
       const oldPath = file.path;
-      const filename = `uuidv4().png`;
+      const filename = `${uuidv4()}.png`;
       const newPath = path.join(public, filename);
         
       const callback = (error) => reject(error);
