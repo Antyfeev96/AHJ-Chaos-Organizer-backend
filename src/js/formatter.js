8700259ab394e25fd5a7e415080ab1b680707595
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-06-02 22:46:12 +0300

    moment added

--- src/js/formatter.js
+++ src/js/formatter.js
@@ -1,12 +1,17 @@
+const moment = require('moment');
+
 const format = () => {
   const date = new Date();
 
-  const formatter = new Intl.DateTimeFormat('ru', {
-   hour: 'numeric',
-   minute: 'numeric',
-  });
+  // const formatter = new Intl.DateTimeFormat('ru', {
+  //  hour: 'numeric',
+  //  minute: 'numeric',
+  //  seconds: 'numeric',
+  // });
+  const ms = date.getMilliseconds();
+  console.log(moment(ms).format('HH:mm:ss'));
 
-  return `${formatter.format(date)}`;
+  return ms.toString();
 };
 
 module.exports.format = format;
